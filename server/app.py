40b7eb7cd2b40c87036979b41ba760107d198ed6
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-22 14:54:35 -0700

    lab complete

--- server/app.py
+++ server/app.py
@@ -46,6 +46,52 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    # def patch(self, id):
+
+    #     plant = Plant.query.filter_by(id=id).first()
+    #     for attr in request.form:
+    #         setattr(plant, attr, request.form[attr])
+
+    #     db.session.add(plant)
+    #     db.session.commit()
+
+    #     # response_dict = plant.to_dict()
+    #     # response = make_response(
+    #     #     response_dict,
+    #     #     200
+    #     # )
+    #     # return response
+    #     return plant.to_dict(), 202
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        # return make_response(plant.to_dict(), 200)
+        return plant.to_dict(), 202
+    
+    def delete(self, id):
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        # response_dict = {"message": "plant successfully deleted"}
+        # response = make_response(
+        #     response_dict,
+        #     200
+        # )
+        # return response
+        return {}, 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
